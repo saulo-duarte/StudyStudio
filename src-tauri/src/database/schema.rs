2f6ba3d1dfd752344a04e0c54148f557e9b7536e
saulo-duarte <saulolduarte@outlook.com>
2025-02-01 11:56:20 -0300

    refactor(database): Remove the last_login from users to the logins

--- src-tauri/src/database/schema.rs
+++ src-tauri/src/database/schema.rs
@@ -7,14 +7,13 @@ pub fn initialize_database(conn: &Connection) -> Result<()> {
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
             status TEXT CHECK(status IN ('active', 'inactive')) NOT NULL,
-            created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-            last_login DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
         );
         
-        CREATE TABLE IF NOT EXISTS logins (
+        CREATE TABLE IF NOT EXISTS user_logins (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             user_id INTEGER NOT NULL,
-            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+            login DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
             FOREIGN KEY (user_id) REFERENCES users(id)
         );
 
