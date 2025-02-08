e66ba96aec645e6c9937afea9e2661c75bf16568
saulo-duarte <saulolduarte@outlook.com>
2025-02-08 14:30:10 -0300

    feat: Add the task commands to the frontend

--- src-tauri/src/commands/mod.rs
+++ src-tauri/src/commands/mod.rs
@@ -1,5 +1,7 @@
 pub mod user_commands;
 pub mod task_commands;
+pub mod window_api;
 
 pub use user_commands::*;
 pub use task_commands::*;
+pub use window_api::*;
--- src-tauri/src/commands/task_commands.rs
+++ src-tauri/src/commands/task_commands.rs
@@ -1,18 +1,49 @@
-use tauri::{command, State};
-use crate::models::{NewTask, Task};
-use crate::AppState;
-use crate::views::display_task;
+use chrono::NaiveDateTime;
+use tauri::State;
 
-#[command]
+use crate::{
+    database::repository::TaskRepository, 
+    errors::TaskError, 
+    models::Task, 
+    views::TaskView, AppState,
+};
+
+#[tauri::command]
 pub fn create_task(
-    state: State<'_, AppState>,
-    new_task: NewTask,
-) -> Result<String, String> {
-    let db_conn = state.db_conn();
-    let mut conn = db_conn.lock().map_err(|e| format!("Failed to acquire database lock: {}", e))?;
+    name: String,
+    user_id: u32,
+    description: Option<String>,
+    due_date: String,
+    state: State<AppState>
+) -> Result<TaskView, String> {
+    let date_replaced = due_date.replace("Z", "");
 
-    let task = Task::create(&mut *conn, new_task)
+    let trimmed_date = if date_replaced.len() > 16 {
+        &date_replaced[..16]
+    } else {
+        &date_replaced
+    };
+
+    let due_date = NaiveDateTime::parse_from_str(trimmed_date, "%Y-%m-%dT%H:%M")
+        .map_err(|e| format!("Invalid date format: {}", e))?;
+    
+    let mut task = Task::new(name, user_id, description, due_date)
+        .map_err(|err| err.to_string())?;
+
+    let db_conn = state.db_conn();
+    let conn = db_conn.lock()
+        .map_err(|_| "Database lock failed")?;
+
+    TaskRepository::insert_task(&conn, &mut task)
         .map_err(|e| format!("Database error: {}", e))?;
 
-    Ok(display_task(&task))
+    Ok(task.into())
+}
+
+#[tauri::command]
+pub fn get_all_tasks(state: State<AppState>) -> Result<Vec<Task>, TaskError> {
+    let db_conn = state.db_conn();
+    let conn = db_conn.lock().map_err(|e| TaskError::DatabaseError(e.to_string()))?;
+    TaskRepository::get_all_tasks(&conn)
+        .map_err(|e| TaskError::DatabaseError(e.to_string()))
 }
